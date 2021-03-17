f96c8c75a1764b9753ad5c7c8ed31ef946cc6e09
nikunj_38 <bholuprajapati3883@gmail.com>
2021-03-17 15:18:05 +0530

    commit at 3.17 PM 17 March 2021

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
+        appBar: AppBar(title: Text("DATA TABLE IN FLUTTER"),centerTitle: true,),
         body: MyStatelessWidget(),
       ),
     );
